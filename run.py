aff1c3ec06e35ed41fab83015c11c44299ea75a7
flashdrag <flashdrag@gmail.com>
2023-02-11 12:30:17 +0000

    install pyfiglet and add header text

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
+from pyfiglet import figlet_format
 
+
+def display_header():
+    print(figlet_format('Library Management System', 'straight'))
+
+
+
+
+
+
+def main():
+    display_header()
+
+
+if __name__ == '__main__':
+    main()

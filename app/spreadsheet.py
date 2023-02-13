eee0c03d8aecc65a6bf91d2f66fe7f3ca259048b
flashdrag <flashdrag@gmail.com>
2023-02-13 15:04:54 +0000

    refactor library: remove worksheet from init

--- app/spreadsheet.py
+++ app/spreadsheet.py
@@ -3,18 +3,22 @@ from google.oauth2.service_account import Credentials
 
 
 class Library:
+    '''
+    A class representing a Library as a spreadsheet, which contains books and methods for managing them.
+    '''
     SCOPE = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive.file",
         "https://www.googleapis.com/auth/drive",
     ]
 
-    def __init__(self, sheet_name, creds_path) -> None:
+    def __init__(self, sheet_name: str, creds_path: str) -> None:
         """
-        :param sheet_name (str): Name of the Google Sheet.
-        :param creds_path (str): Path to credentials file.
+        Initialize a connection to the Google Sheet specified in the `sheet_name` parameter, using the credentials
+        in the file at `creds_path`.
+        :param sheet_name: Name of the Google Sheet.
+        :param creds_path: Path to credentials file.
         """
-        self.sheet_name = sheet_name
-        scope = Credentials.from_service_account_file(self.creds_path, scopes=self.SCOPE)
+        scope = Credentials.from_service_account_file(creds_path, scopes=self.SCOPE)
         client = gspread.authorize(scope)
-        self.stock_sheet = client.open(self.sheet_name).worksheet('stock')
+        self._SHEET = client.open(sheet_name)

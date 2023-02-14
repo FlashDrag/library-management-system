68c2f37dff5b3c7e7db8ff8b055c8d5677fdcfe5
flashdrag <flashdrag@gmail.com>
2023-02-14 21:29:33 +0000

    separate 'font' and 'clear terminal' to 'tools' module

---
+++ app/services/tools.py
@@ -0,0 +1,20 @@
+import os
+
+
+class font:
+    BOLD = '\033[1m'
+    ITALIC = '\033[3m'
+    HEADER = '\033[92m'
+    ERROR = '\033[91m'
+    YELLOW = '\033[93m'
+    UNDERLINE = '\033[4m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    GREY = '\033[90m'
+    ENDC = '\033[0m'
+
+
+def clear_terminal():
+    '''Clear terminal screen'''
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")

b25ab848265fd8cd722e2882ff617833d1489bb4
flashdrag <flashdrag@gmail.com>
2023-03-03 22:50:58 +0000

    Change output table title color to purple

--- library_system/views/console_ui.py
+++ library_system/views/console_ui.py
@@ -70,7 +70,7 @@ class Menu:
         (0, 1) - no padding on top and bottom, 1 space on the right and left.
         '''
 
-        title_style = Style(color='green')
+        title_style = Style(color='purple')
         table = Table(
             title=title,
             title_style=title_style,
